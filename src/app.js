c9b94eb05a64ea0a59ab1f3d426cf5c30d0982f7
GusTeixeira <gusteixeira3004@gmail.com>
2022-08-07 02:46:02 -0300

    Adicionando rotas e controllers

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express"
 import db from "./config/dbConnect.js"
+import livros from "./models/Livros.js"
+import routes from "./routes/index.js"
 
 db.on("error", console.log.bind(console, 'Erro de conexão'))
 db.once("open", () => {
@@ -10,40 +12,7 @@ const app = express();
 
 app.use(express.json())
 
-const testes = [
-    {id:1, "nome": "Gus"},
-    {id:2, "nome": "Robin"}
-]
-
-app.get('/', (req, res) => {
-    res.status(200).send('Grande dia!');
-})
-
-app.get('/testes', (req,res) =>{
-    res.status(200).json(testes);
-})
-
-app.get('/testes/:id', (req,res) =>{
-    let index = buscatestes(req.params.id);
-    res.json(testes[index]);
-})
-
-app.post('/testes',(req,res)=>{
-    testes.push(req.body);
-    res.status(201).send('Funcionou caralho');
-})
-
-app.put('/testes/:id', (req,res)=>{
-     let index = buscatestes(req.params.id);
-     testes[index].nome = req.body.nome;
-     res.json(testes);   
-})
-
-app.delete('/testes/:id', (req,res)=>{
-    let index = buscatestes(req.params.id);
-    testes.splice(index, 1);
-    res.send(`Teste ${req.params.id} removido com sucesso`);   
-})
+routes(app);
 
 //função que busca os testes pelo ID
 function buscatestes(id){
--- src/models/Livros.js
+++ src/models/Livros.js
@@ -6,5 +6,8 @@ titulo:{type:String, required:true},
 autor:{type:Number},
 editora:{type:Number},
 numeroPaginas:{type:Number}
-}
-)
+});
+
+const livros = mongoose.model('livros', livrosSchema);
+
+export default livros;
