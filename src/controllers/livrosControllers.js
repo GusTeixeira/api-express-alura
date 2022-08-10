e8e14b111e0a3bcc1b81968412b954bfe6740d00
GusTeixeira <gusteixeira3004@gmail.com>
2022-08-10 18:43:20 -0300

    Adicionando CRUD completo

--- src/controllers/livrosControllers.js
+++ src/controllers/livrosControllers.js
@@ -9,6 +9,18 @@ class LivroController{
         })
     }
 
+    static listarLivrosPorID = (req, res) =>{
+        const id = req.params.id;
+
+        livros.findById(id, (err, livros)=>{
+            if(err){
+                res.status(400).send({message: `${err.message} - ID não encontrado`})
+            } else {
+                res.status(200).send(livros)
+            }
+        })
+    }
+
     static cadastrarLivros = (req, res) =>{
         let livro = new livros(req.body);
         livro.save((err)=> {
@@ -19,6 +31,30 @@ class LivroController{
             }
         })
     }
+
+    static atualizarLivros = (req, res) => {
+        const id = req.params.id;
+        livros.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+            if(err){
+                res.status(500).send({message: `${err.message} - falha ao atualizar`})
+            } else{
+                res.status(200).send({message: 'Atualizado com sucesso'})
+            }
+        })
+    }
+
+    static excluirLivros = (req, res) => {
+        const id = req.params.id;
+
+        livros.findByIdAndDelete(id, (err) => {
+            if(err){
+                res.status(500).send({message: `${err.message} - falha ao excluir`})
+            } else{
+                res.status(200).send({message: 'Livro removido com sucesso'})
+            }
+        })
+
+    }    
 
 }
 
